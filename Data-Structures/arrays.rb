b91033329758afa87433f6f0217e9d867a246a48
kwambiee <kwambokaj2.jk@gmail.com>
2023-06-13 17:36:08 +0300

    first commit

---
+++ Data-Structures/arrays.rb
@@ -0,0 +1,5 @@
+# An array is a type of data structure that stores elements of the same type in a contiguous block of memory.
+def reverseArray(arr)
+    arr.reverse
+end
+p reverseArray([3,6,8])
---
+++ Data-Structures/hourglass.rb
@@ -0,0 +1,12 @@
+def hourglassSum(arr)
+    max = 0;
+    for i in 0..3
+        for j in 0..3
+            sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+            max = sum if sum > max
+        end
+    end
+    max
+end
+
+p hourglassSum([[-9,-9,-9,1,1,1],[0,-9,0,4,3,2],[-9,-9,-9,1,2,3],[0,0,8,6,6,0],[0,0,0,-2,0,0],[0,0,1,2,4,0]])
